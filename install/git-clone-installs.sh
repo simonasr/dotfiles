e8008a7c0589c9227e282d858fa14dbd62ab4628
Simonas Rupsys <simmonas.rupsys@vinted.com>
2019-09-16 15:11:27 +0300

    Kitty themes

--- install/git-clone-installs.sh
+++ install/git-clone-installs.sh
@@ -2,3 +2,6 @@
 
 # Install zgen
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
+
+# Kitty themes
+git clone --depth 1 git@github.com:dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
