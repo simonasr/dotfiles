82d596a2d8b94046dc6d5f4f8bd3a64cdc9ca6ce
Simonas Rupsys <simmonas.rupsys@vinted.com>
2022-07-07 17:42:24 +0300

    fix git clone installs

--- install/git-clone-installs.sh
+++ install/git-clone-installs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Kitty themes
 sudo chown -R sirup ~/.config/
+sudo chown -R sirup ~/.local/share
+
+# Kitty themes
 git clone --depth 1 git@github.com:dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
 
 # goenv
 git clone https://github.com/syndbg/goenv.git ~/.goenv
 
 # pip
-pip3 install dotdrop/requirements.txt
+pip3 install -r dotdrop/requirements.txt
